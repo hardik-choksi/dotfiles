1bbe48718668f6ffb7aac5d5becc197c6f2d80cd
hardik@example.com <>
2026-07-16 19:54:38 +0530

    sync nix

--- nixos/home.nix
+++ nixos/home.nix
@@ -41,7 +41,7 @@
   programs.zsh = {
     enable = true;
     enableCompletion = true;
-    autosuggestion.enable = true;
+    autosuggestion.enable = false;
     syntaxHighlighting.enable = true;
 
     oh-my-zsh = {
